b2fb516a874a745d1942cd03c472699a3e2bc784
Qwekem482 <Qwekem4802@gmail.com>
2021-09-26 16:15:58 +0700

    Word Class submit 1

--- src/main/java/Word.java
+++ src/main/java/Word.java
@@ -1,2 +1,39 @@
 public class Word {
+    private String word_target
+    private String word_explain;
+
+    public void setWord_explain(String word_explain) {
+        this.word_explain = word_explain;
+    }
+
+    public void setWord_target(String word_target) {
+        this.word_target = word_target;
+    }
+
+    public String getWord_explain() {
+        return word_explain;
+    }
+
+    public String getWord_target() {
+        return word_target;
+    }
+
+    /**
+     * Constructor 1.
+     * @param word_target: Word in English
+     * @param word_explain: Meaning in Vietnamese
+     */
+    public Word(String word_target, String word_explain) {
+        this.word_target = word_target;
+        this.word_explain = word_explain;
+    }
+
+    /**
+     * Constructor 2.
+     * @param w1: Another word
+     */
+    public Word(Word w1) {
+        this.word_target = w1.word_target;
+        this.word_explain = w1.word_explain;
+    }
 }
