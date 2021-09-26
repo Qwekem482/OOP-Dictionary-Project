a990e200337fbf2d59f0ad8b742ecf556d968b3c
Qwekem482 <Qwekem4802@gmail.com>
2021-09-26 21:58:31 +0700

    Update DictionaryManagement.java

--- src/main/java/DictionaryManagement.java
+++ src/main/java/DictionaryManagement.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class DictionaryManagement {
+    private Word w1 = new Word();
+    private Dictionary dict = new Dictionary();
+
+    private void insertWord(Scanner input) {
+        w1.setWord_target(input.nextLine());
+        w1.setWord_explain(input.nextLine());
+        dict.addWordToMap(w1); //Need to rewrite after has trie dictionary
+    }
+
+    public void insertFromCommandline() {
+        Scanner input = new Scanner(System.in);
+        while (input.hasNextLine()) {
+            insertWord(input);
+        }
+        insertWord(input);
+    }
 
 }
