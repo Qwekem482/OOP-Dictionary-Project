b46e7d05e17f4cf4d49e5897110ab2122fc8c3f9
Thais-le <80798567+letthai@users.noreply.github.com>
2021-09-28 20:24:01 +0700

    Update DictionaryCommandline.java

--- src/main/java/DictionaryCommandline.java
+++ src/main/java/DictionaryCommandline.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+import java.util.TreeMap;
+
 public class DictionaryCommandline {
+    private DictionaryManagement _word = new DictionaryManagement();
+    /**
+     * show all the word in the array
+     */
+    public void Show(){
+        _word.ShowWord();
+    }
+    /**
+     * //function DictionaryBasic()
+     * Insert Word and show the all the word in the array
+     */
+    public void DictionaryBasic(){
+        _word.insertFromCommandline();
+        _word.ShowWord();
+    }
 }
